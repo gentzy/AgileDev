b414bd6e672cf3837d2717369214ce645e2f5360
Gentiana Coman <gentiana.coman@hotmail.com>
2015-03-08 22:36:37 -0700

    CSharpBasics assignment
    Adding the C# Basics assignment stub

---
+++ Courses/C#/Assignments/CSharpBasics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteRightTriangle("Combinations.txt");
+        }
+
+        // Write the code to solve the assignment here
+        public static void ExecuteRightTriangle(string pathToFile)
+        {
+        }
+    }
+}
