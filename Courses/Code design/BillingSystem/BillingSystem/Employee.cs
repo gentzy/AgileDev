678927ccf5b2fbb1f2b1b3e6729c4d7ab33d6f83
Gentiana Coman <gentiana.coman@hotmail.com>
2015-03-11 22:24:45 -0700

    Adding the BillingSystem assignment
    Assignment code for the code qualities lesson.

---
+++ Courses/Code design/BillingSystem/BillingSystem/Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BillingSystem
+{
+    public class Employee
+    {
+        string m_name;
+        string m_title;
+        double m_billingRate; // per hour
+
+        public Employee() { }
+
+        public void BillCustomer(int numberOfHours, Bill customerBill)
+        {
+            customerBill.AddBillAmount(numberOfHours * m_billingRate);
+        }
+    }
+
+    public class Client
+    {
+        string m_name;
+        string m_billingAddress;
+        double m_billingRate;
+        CreditCard m_creditCard;
+
+        public void Pay(double amount)
+        {
+            m_creditCard.CreditAmount(amount);
+        }
+    }
+
+    public class CreditCard
+    {
+        string m_cardNumber;
+        string m_cardType;
+        string m_cardSecurityCode;
+
+        public void CreditAmount(double amount)
+        {
+            // Send information to credit card server
+        }
+    }
+
+    public class WorkOrder
+    {
+        Client client;
+        Employee employee;
+        int numberOfHours;
+    }
+
+    public class Bill
+    {
+        List<WorkOrder> workOrders;
+
+        public double CreateTotalBill()
+        {
+            double total = 0;
+            foreach (double amount in m_amounts)
+            {
+                total += amount;
+            }
+
+            return total;
+        }
+
+        public void AddBillAmount(double amount)
+        {
+            m_amounts.Add(amount);
+        }
+    }
+}
